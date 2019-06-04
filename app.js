6f4dbd6796ecd8b6e8fe5f0b3a9d3dd5f912d845
Mark Katerberg <katerberg@fastmail.fm>
2019-06-03 23:01:46 -0700

    Makes calls a little clearer

--- app.js
+++ app.js
@@ -13,28 +13,37 @@ const options = {
   identity,
   channels: [channelName]
 };
+
 const client = new tmi.client(options);
-let isQueueOpen = false;
 
 const connect = client.connect().then(msg => {
   console.log('Connected!');
 });
 
+function unpermissioned(channelName, message) {
+  if (message.startsWith('!twitter')) {
+    return client.say(channelName, 'Want to argue about VRD? https://twitter.com/stlvrd');
+  } else if (message.startsWith('!salt')) {
+    return client.say(channelName, 'PJSalt PJSalt PJSalt PJSalt');
+  }
+}
+
+function mods(channelName, message, user) {
+  const isOwner = user.username === channelName;
+  const isDope = user.mod || isOwner;
+  if ((message.startsWith('!so') || message.startsWith('!shoutout')) && isDope) {
+    const msgArr = message.split(' ');
+    return client.say(channelName, `Check out http://twitch.tv/${msgArr[1]} for some really cool content!`);
+  }
+}
+
 client.on('chat', (channel, user, message, self) => {
   try {
     if (self) return;
 
-    const isOwner = user.username === channelName;
-    const isDope = user.mod || isOwner;
-    const msgArr = message.split(' ');
+    unpermissioned(channelName, message) ||
+      mods(channelName, message, user);
 
-    if ((message.startsWith('!so') || message.startsWith('!shoutout')) && isDope) {
-      client.say(channelName, `Check out http://twitch.tv/${msgArr[1]} for some really cool content!`);
-    } else if (message.startsWith('!twitter')) {
-      client.say(channelName, 'Want to argue about VRD? https://twitter.com/stlvrd');
-    } else if (message.startsWith('!salt')) {
-      client.say(channelName, 'PJSalt PJSalt PJSalt PJSalt');
-    }
   } catch(e) {
     console.error('Something went wrong trying to parse the message');
     console.log(e.message);
