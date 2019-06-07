66e4b0aae15107e5f04561ec9b99d4f2196a9e27
Mark Katerberg <katerberg@fastmail.fm>
2019-06-06 22:47:42 -0700

    Allows users to vote for who will win

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const tmi = require('tmi.js');
 const identity = require('./creds/twitchCreds.json');
+const dbInfo = require('./creds/dbCreds.json');
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  ...dbInfo,
+});
+
+connection.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to DB');
+});
+
+connection.on('error', function() {
+  console.log('something went terribly wrong');
+});
 
 const channelName = 'stlvrd';
 const options = {
@@ -17,8 +33,64 @@ const options = {
 const client = new tmi.client(options);
 
 const connect = client.connect().then(msg => {
-  console.log('Connected!');
+  console.log(`Connected to ${channelName}!`);
 });
+
+async function getVotes(username) {
+  return new Promise((res, rej) => {
+    connection.query('SELECT count(vote) AS voteCount FROM votes WHERE username = ?;', [username], (err, result) => {
+      if (err) {
+        console.error(`Error retrieving vote count for ${username}`);
+        return rej(err);
+      }
+      res(result.voteCount);
+    });
+  });
+}
+
+async function insertVote(username, vote) {
+  return new Promise((res, rej) => {
+    connection.query('INSERT INTO votes (username, vote) VALUES (?, ?);', [username, vote], (err, result) => {
+      if (err) {
+        console.error(err);
+        console.error(`Something went wrong inserting vote for ${username}`);
+        rej(err);
+      } else {
+        console.log('Vote inserted');
+        res(vote);
+      }
+    });
+  });
+}
+
+async function updateVote(username, vote) {
+  return new Promise((res, rej) => {
+    connection.query('UPDATE votes SET vote = ? WHERE username = ?;', [vote, username], (err, result) => {
+      if (err) {
+        console.error(err);
+        console.error(`Something went wrong updating vote for ${username}`);
+        rej(err);
+      } else {
+        console.log('Vote updated');
+        res(vote);
+      }
+    });
+  });
+}
+
+async function upsertVote(username, message) {
+  const count = await getVotes(username);
+  const voteFor = message.split(' ')[1];
+  if (count <= 1) {
+    return insertVote(username, voteFor);
+  } else {
+    return updateVote(username, voteFor);
+  }
+}
+
+function getVotesQuery() {
+  return 'SELECT username, COUNT(vote) FROM votes GROUP BY username;';
+}
 
 function unpermissioned(channelName, message) {
   if (message.startsWith('!twitter')) {
@@ -36,17 +108,31 @@ function unpermissioned(channelName, message) {
     message.startsWith('!decklists') ||
     message.startsWith('!draft')) {
     return client.say(channelName, 'https://docs.google.com/spreadsheets/d/1MKkuuQ1hYIE4_uOXEUBUcTMSu1yfsgwYgHjIGyNQtm4/edit?usp=sharing');
-  } else if (message.startsWith('!youtube') {
+  } else if (message.startsWith('!youtube')) {
     return client.say(channelName, 'https://www.youtube.com/channel/UCpwS9X2A-5pmo1txhyD7eoA');
+  } else if (message === '!votes') {
+    connection.query(getVotesQuery(), (err, result) => {
+      if (err) {
+        console.error(err);
+        console.error('Something went wrong getting votes');
+      } else {
+        console.log(result);
+      }
+    });
+    return;
   }
 }
 
 function mods(channelName, message, user) {
   const isOwner = user.username === channelName;
   const isDope = user.mod || isOwner;
-  if ((message.startsWith('!so') || message.startsWith('!shoutout')) && isDope) {
-    const msgArr = message.split(' ');
-    return client.say(channelName, `Check out http://twitch.tv/${msgArr[1]} for some really cool content!`);
+  if (isDope) {
+    if (message.startsWith('!so') || message.startsWith('!shoutout')) {
+      const msgArr = message.split(' ');
+      return client.say(channelName, `Check out http://twitch.tv/${msgArr[1]} for some really cool content!`);
+    } else if(message.startsWith('!vote ')) {
+      upsertVote(user.username, message);
+    }
   }
 }
 
