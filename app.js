c6fcc83b252dc9adbeefd7ad5f8541b42faabdb5
Mark Katerberg <katerberg@fastmail.fm>
2019-06-07 18:18:31 -0700

    Cleans up more startsWith

--- app.js
+++ app.js
@@ -23,28 +23,29 @@ client.connect().then(msg => {
 });
 
 function unpermissioned(channelName, message, user) {
-  if (message.startsWith('!twitter')) {
+  if (message === '!twitter') {
     return client.say(channelName, 'Want to argue about VRD? https://twitter.com/stlvrd');
-  } else if (message.startsWith('!salt')) {
+  } else if (message === '!salt') {
     return client.say(channelName, 'PJSalt PJSalt PJSalt PJSalt');
-  } else if (message.startsWith('!vrd1')) {
+  } else if (message === '!vrd1') {
     return client.say(channelName, 'https://docs.google.com/spreadsheets/d/1axKhYoW4HanXaSnp1UXZQkOS-p_HUrcLrL45qzfkMdE/edit?usp=sharing');
-  } else if (message.startsWith('!bracket') ||
-    message.startsWith('!record')) {
+  } else if (message === '!bracket' ||
+    message === '!record') {
     return client.say(channelName, 'https://challonge.com/yvl5j2oj/standings');
   } else if (message === '!vrd2' ||
     message === '!sheet' ||
+    message === '!picks' ||
     message === '!decklist' ||
     message === '!decklists' ||
     message === '!draft') {
-    return client.say(channelName, 'https://docs.google.com/spreadsheets/d/1MKkuuQ1hYIE4_uOXEUBUcTMSu1yfsgwYgHjIGyNQtm4/edit?usp=sharing');
-  } else if (message.startsWith('!youtube')) {
-    return client.say(channelName, 'https://www.youtube.com/channel/UCpwS9X2A-5pmo1txhyD7eoA');
+    return client.say(channelName, 'Spreadsheet with draft picks is at https://docs.google.com/spreadsheets/d/1MKkuuQ1hYIE4_uOXEUBUcTMSu1yfsgwYgHjIGyNQtm4/edit?usp=sharing');
+  } else if (message === '!youtube') {
+    return client.say(channelName, 'Find our VODs on Twitch or on https://www.youtube.com/channel/UCpwS9X2A-5pmo1txhyD7eoA');
   } else if(message.startsWith('!win ')) {
     dao.upsertWinVote(user.username, message);
   } else if (message === '!wins') {
     return dao.getAllWinVotes().then(votes => {
-      const votesMessage = votes.reduce((a, c) => `${a}\r\n${c.candidate} has ${c.votes} vote${c.votes > 1 ? 's' : ''}.`, '');
+      const votesMessage = votes.reduce((a, c) => `${a}\r\n${c.votes} vote${c.votes > 1 ? 's' : ''} for ${c.candidate} to win.`, '');
       client.say(channelName, votesMessage ? votesMessage : 'No votes yet!');
     });
   }
@@ -54,9 +55,9 @@ function mods(channelName, message, user) {
   const isOwner = user.username === channelName;
   const isDope = user.mod || isOwner;
   if (isDope) {
-    if (message.startsWith('!so') || message.startsWith('!shoutout')) {
+    if (message === '!so' || message === '!shoutout') {
       return client.say(channelName, `Check out http://twitch.tv/${getCommandParams(message)} for some really cool content!`);
-    } else if (message.startsWith('!clearVotes')) {
+    } else if (message === '!clearWinVotes') {
       return dao.clearAllWinVotes();
     }
   }
