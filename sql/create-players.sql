41b745b4fb5b8b2ade8b105d156a998d05dcbecf
Mark Katerberg <katerberg@fastmail.fm>
2019-06-07 17:19:49 -0700

    Adds players table

---
+++ sql/create-players.sql
@@ -0,0 +1,12 @@
+DROP TABLE players;
+CREATE TABLE players (name VARCHAR(200), shortName VARCHAR(200));
+ALTER TABLE players ADD CONSTRAINT players_constraint UNIQUE (shortName);
+
+INSERT INTO players (name, shortName) VALUES ('Naveen Balaji', 'naveen');
+INSERT INTO players (name, shortName) VALUES ('Jeff Blyden', 'jeff');
+INSERT INTO players (name, shortName) VALUES ('Brent Yard', 'brent');
+INSERT INTO players (name, shortName) VALUES ('Eric Levine', 'eric');
+INSERT INTO players (name, shortName) VALUES ('Daniel Zielinski', 'daniel');
+INSERT INTO players (name, shortName) VALUES ('Vincent Brown', 'vincent');
+INSERT INTO players (name, shortName) VALUES ('Stephen Hagan', 'stephen');
+INSERT INTO players (name, shortName) VALUES ('Elaine Cao', 'elaine');
