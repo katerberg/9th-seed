f5c563e92fb1ee40be5428b72764fa7e4f315185
Mark Katerberg <katerberg@fastmail.fm>
2019-06-07 20:32:39 -0700

    Adds foreign keys and orders SQL runs

--- sql/20-create-wins.sql
+++ sql/20-create-wins.sql
@@ -1,3 +1,4 @@
 DROP TABLE wins;
 CREATE TABLE wins (username VARCHAR(200), vote VARCHAR(200));
 ALTER TABLE wins ADD CONSTRAINT wins_unique_constraint UNIQUE (username);
+ALTER TABLE wins ADD CONSTRAINT vote_for_players_constraint FOREIGN KEY players_constraint (vote) REFERENCES players (shortName);
