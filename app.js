3d2b4aa47231688ae8ba3280994b21a2b79d1454
Mark Katerberg <katerberg@fastmail.fm>
2019-06-08 21:55:46 -0500

    Accidentally let atom reformat all of my code to look even worse than usual

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const tmi = require('tmi.js');
-const identity = require('./creds/twitchCreds.json');
-const dbInfo = require('./creds/dbCreds.json');
-const {WinsDataAccessObject} = require('./winsDao');
-const {getCommandParams}  = require('./utils');
-const mysql = require('mysql');
+const tmi = require("tmi.js");
+const identity = require("./creds/twitchCreds.json");
+const dbInfo = require("./creds/dbCreds.json");
+const { WinsDataAccessObject } = require("./winsDao");
+const { getCommandParams } = require("./utils");
+const mysql = require("mysql");
 
-const channelName = 'stlvrd';
+const channelName = "stlvrd";
 const tmiOptions = {
   option: {
     debug: true
@@ -18,56 +18,83 @@ const tmiOptions = {
 };
 
 const connection = mysql.createConnection({
-  ...dbInfo,
+  ...dbInfo
 });
 
 connection.connect(err => {
   if (err) {
     throw err;
   }
-  console.log('Connected to DB');
+  console.log("Connected to DB");
 });
 
-connection.on('error', () => {
-  console.error('something went terribly wrong connecting to mysql');
+connection.on("error", () => {
+  console.error("something went terribly wrong connecting to mysql");
 });
 
 const winsDao = new WinsDataAccessObject(connection);
 const client = new tmi.client(tmiOptions);
 
-client.connect().then(msg => {
-  console.log(`Connected to ${channelName}!`);
-}).catch(e => {
-  console.error('Error connecting to Twitch');
-});
+client
+  .connect()
+  .then(msg => {
+    console.log(`Connected to ${channelName}!`);
+  })
+  .catch(e => {
+    console.error("Error connecting to Twitch");
+  });
 
 function unpermissioned(channelName, message, user) {
-  if (message === '!twitter') {
-    return client.say(channelName, 'Want to argue about VRD? https://twitter.com/stlvrd');
-  } else if (message === '!salt') {
-    return client.say(channelName, 'PJSalt PJSalt PJSalt PJSalt');
-  } else if (message === '!vrd1') {
-    return client.say(channelName, 'https://docs.google.com/spreadsheets/d/1axKhYoW4HanXaSnp1UXZQkOS-p_HUrcLrL45qzfkMdE/edit?usp=sharing');
-  } else if (message === '!bracket' ||
-    message === '!record') {
-    return client.say(channelName, 'https://challonge.com/yvl5j2oj/standings');
-  } else if (message === '!vrd2' ||
-    message === '!sheet' ||
-    message === '!picks' ||
-    message === '!decklist' ||
-    message === '!decklists' ||
-    message === '!draft') {
-    return client.say(channelName, 'Spreadsheet with draft picks is at https://docs.google.com/spreadsheets/d/1MKkuuQ1hYIE4_uOXEUBUcTMSu1yfsgwYgHjIGyNQtm4/edit?usp=sharing');
-  } else if (message === '!youtube') {
-    return client.say(channelName, 'Find our VODs on Twitch or on https://www.youtube.com/channel/UCpwS9X2A-5pmo1txhyD7eoA');
-  } else if(message.startsWith('!win ')) {
+  if (message === "!twitter") {
+    return client.say(
+      channelName,
+      "Want to argue about VRD? https://twitter.com/stlvrd"
+    );
+  } else if (message === "!salt") {
+    return client.say(channelName, "PJSalt PJSalt PJSalt PJSalt");
+  } else if (message === "!vrd1") {
+    return client.say(
+      channelName,
+      "https://docs.google.com/spreadsheets/d/1axKhYoW4HanXaSnp1UXZQkOS-p_HUrcLrL45qzfkMdE/edit?usp=sharing"
+    );
+  } else if (message === "!bracket" || message === "!record") {
+    return client.say(channelName, "https://challonge.com/yvl5j2oj/standings");
+  } else if (
+    message === "!vrd2" ||
+    message === "!sheet" ||
+    message === "!picks" ||
+    message === "!decks" ||
+    message === "!deck" ||
+    message === "!decklist" ||
+    message === "!decklists" ||
+    message === "!draft"
+  ) {
+    return client.say(
+      channelName,
+      "Spreadsheet with draft picks is at https://docs.google.com/spreadsheets/d/1MKkuuQ1hYIE4_uOXEUBUcTMSu1yfsgwYgHjIGyNQtm4/edit?usp=sharing"
+    );
+  } else if (message === "!youtube") {
+    return client.say(
+      channelName,
+      "Find our VODs on Twitch or on https://www.youtube.com/channel/UCpwS9X2A-5pmo1txhyD7eoA"
+    );
+  } else if (message.startsWith("!win ")) {
     winsDao.upsertWinVote(user.username, message).catch(e => {
-      client.say(channelName, 'Error: votes can only be for players in the tournament. Try "!win naveen" instead');
+      client.say(
+        channelName,
+        'Error: votes can only be for players in the tournament. Try "!win naveen" instead'
+      );
     });
-  } else if (message === '!wins') {
+  } else if (message === "!wins") {
     return winsDao.getAllWinVotes().then(wins => {
-      const winsMessage = wins.reduce((a, c) => `${a}\r\n${c.wins} vote${c.wins > 1 ? 's' : ''} for ${c.candidate} to win.`, '');
-      client.say(channelName, winsMessage ? winsMessage : 'No votes yet!');
+      const winsMessage = wins.reduce(
+        (a, c) =>
+          `${a}\r\n${c.wins} vote${c.wins > 1 ? "s" : ""} for ${
+            c.candidate
+          } to win.`,
+        ""
+      );
+      client.say(channelName, winsMessage ? winsMessage : "No votes yet!");
     });
   }
 }
@@ -76,23 +103,27 @@ function mods(channelName, message, user) {
   const isOwner = user.username === channelName;
   const isDope = user.mod || isOwner;
   if (isDope) {
-    if (message === '!so' || message === '!shoutout') {
-      return client.say(channelName, `Check out http://twitch.tv/${getCommandParams(message)} for some really cool content!`);
-    } else if (message === '!clearWinVotes') {
+    if (message === "!so" || message === "!shoutout") {
+      return client.say(
+        channelName,
+        `Check out http://twitch.tv/${getCommandParams(
+          message
+        )} for some really cool content!`
+      );
+    } else if (message === "!clearWinVotes") {
       return winsDao.clearAllWinVotes();
     }
   }
 }
 
-client.on('chat', (channel, user, message, self) => {
+client.on("chat", (channel, user, message, self) => {
   try {
     if (self) return;
 
     unpermissioned(channelName, message, user) ||
       mods(channelName, message, user);
-
-  } catch(e) {
-    console.error('Something went wrong trying to parse the message');
+  } catch (e) {
+    console.error("Something went wrong trying to parse the message");
     console.log(e.message);
   }
 });
