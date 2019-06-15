9b881a0201c4beae51f8e7bd505d0de6766ee711
Mark Katerberg <katerberg@fastmail.fm>
2019-06-14 23:44:00 -0500

    Reformats SQL for smaller horizontal space

--- sql/20-create-wins.sql
+++ sql/20-create-wins.sql
@@ -1,4 +1,13 @@
 DROP TABLE votes;
-CREATE TABLE votes (username VARCHAR(200), candidate VARCHAR(200), category VARCHAR(200));
-ALTER TABLE votes ADD CONSTRAINT votes_unique_constraint UNIQUE (username, category);
-ALTER TABLE votes ADD CONSTRAINT candidate_to_player_fk FOREIGN KEY players_constraint (candidate) REFERENCES players (shortName);
+CREATE TABLE votes (
+  username VARCHAR(200),
+  candidate VARCHAR(200),
+  category VARCHAR(200)
+);
+ALTER TABLE votes
+  ADD CONSTRAINT votes_unique_constraint
+  UNIQUE (username, category);
+ALTER TABLE votes
+  ADD CONSTRAINT candidate_to_player_fk
+  FOREIGN KEY players_constraint (candidate)
+  REFERENCES players (shortName);
