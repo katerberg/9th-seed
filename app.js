a1136b6070c2e44940a264cc69c9b67cd5153be1
Mark Katerberg <katerberg@fastmail.fm>
2019-06-14 22:26:30 -0500

    Extracted client.say to prevent more copy-pasta

--- app.js
+++ app.js
@@ -44,21 +44,21 @@ client
     console.error("Error connecting to Twitch");
   });
 
+function say(message) {
+  client.say(channelName, message);
+}
+
 function unpermissioned(channelName, message, user) {
   if (message === "!twitter") {
-    return client.say(
-      channelName,
-      "Want to argue about VRD? https://twitter.com/stlvrd"
-    );
+    return say("Want to argue about VRD? https://twitter.com/stlvrd");
   } else if (message === "!salt") {
-    return client.say(channelName, "PJSalt PJSalt PJSalt PJSalt");
+    return say("PJSalt PJSalt PJSalt PJSalt");
   } else if (message === "!vrd1") {
-    return client.say(
-      channelName,
+    return say(
       "https://docs.google.com/spreadsheets/d/1axKhYoW4HanXaSnp1UXZQkOS-p_HUrcLrL45qzfkMdE/edit?usp=sharing"
     );
   } else if (message === "!bracket" || message === "!record") {
-    return client.say(channelName, "https://challonge.com/yvl5j2oj/standings");
+    return say("https://challonge.com/yvl5j2oj/standings");
   } else if (
     message === "!vrd2" ||
     message === "!sheet" ||
@@ -69,18 +69,15 @@ function unpermissioned(channelName, message, user) {
     message === "!decklists" ||
     message === "!draft"
   ) {
-    return client.say(
-      channelName,
+    return say(
       "Spreadsheet with draft picks is at https://docs.google.com/spreadsheets/d/1MKkuuQ1hYIE4_uOXEUBUcTMSu1yfsgwYgHjIGyNQtm4/edit?usp=sharing"
     );
   } else if (message === "!youtube") {
-    return client.say(
-      channelName,
+    return say(
       "Find our VODs on Twitch or on https://www.youtube.com/channel/UCpwS9X2A-5pmo1txhyD7eoA"
     );
   } else if (message === "!hs") {
-    return client.say(
-      channelName,
+    return say(
       "HeartSupport is a safe place online to talk about depression, anxiety, suicidal thoughts, eating disorders, self-harm, addictions or anything else that's hard. Catch the IRL stream talking about these kinds of issues at twitch.tv/heartsupport - MORE INFO: www.heartsupport.com"
     );
   } else if (message.startsWith("!win ")) {
@@ -91,14 +88,12 @@ function unpermissioned(channelName, message, user) {
           const playerList = players.reduce((a, c) => {
             return a ? `${a}, ${c.shortName}` : c.shortName;
           }, "");
-          return client.say(
-            channelName,
+          return say(
             `I don't know who you're voting for. Try voting for one of these players: ${playerList}`
           );
         })
         .catch(() => {
-          return client.say(
-            channelName,
+          return say(
             'Error: votes can only be for players in the tournament. Try "!win naveen" instead'
           );
         });
@@ -112,10 +107,7 @@ function unpermissioned(channelName, message, user) {
           } to win.`,
         ""
       );
-      return client.say(
-        channelName,
-        winsMessage ? winsMessage : "No votes yet!"
-      );
+      return say(winsMessage ? winsMessage : "No votes yet!");
     });
   }
 }
@@ -125,8 +117,7 @@ function mods(channelName, message, user) {
   const isDope = user.mod || isOwner;
   if (isDope) {
     if (message === "!so" || message === "!shoutout") {
-      return client.say(
-        channelName,
+      return say(
         `Check out http://twitch.tv/${getCommandParams(
           message
         )} for some really cool content!`
