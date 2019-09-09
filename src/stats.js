db7891a79f62c377256a04dde6b7dd646eb3f301
Mark Katerberg <katerberg@fastmail.fm>
2019-09-08 22:43:28 -0500

    Hotfix for broken number of drafts

--- src/stats.js
+++ src/stats.js
@@ -5,7 +5,7 @@ const {isValidCardName} = require('./oracleDao');
 function getMessage(card, numberTaken, average) {
   const roundedAverage = Math.round(average * 10) / 10;
   const pickRound = Math.ceil(roundedAverage / 8);
-  return `${card} has been picked ${numberTaken} time${numberTaken > 1 ? 's' : ''} (of 13) at pick ${roundedAverage} (round ${pickRound}) on average`;
+  return `${card} has been picked ${numberTaken} time${numberTaken > 1 ? 's' : ''} (of 14) at pick ${roundedAverage} (round ${pickRound}) on average`;
 }
 
 async function fuzzyMatch(card) {
