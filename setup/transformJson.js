8bd037167cafbb9de11530e0bf1fdff1d818ac2d
Mark Katerberg <katerberg@fastmail.fm>
2019-11-24 22:33:03 -0600

    Adds STLVRD 4 and allows card lists to be fetched on demand

--- setup/transformJson.js
+++ setup/transformJson.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 const util = require('util');
+const stringify = require('csv-stringify')
 
 fs.readFileAsync = util.promisify(fs.readFile);
 fs.writeFileAsync = util.promisify(fs.writeFile);
+const stringifyAsync = util.promisify(stringify);
 
 
 fs.readFileAsync(`${process.cwd()}/setup/VintageCards.json`, 'utf-8').then((textFile) => {
-  const cards = JSON.parse(textFile);
-  const cardNames = JSON.stringify(Object.keys(cards)).slice(1, -1);
-  fs.writeFile(`${process.cwd()}/setup/AllCards.json`, cardNames, (err) => {
-    if (err) {
-      throw err;
-    }
-  });
+  const cards = Object.keys(JSON.parse(textFile));
+  stringifyAsync(cards.map(c => [c])).then((output) => {
+    fs.writeFile(`${process.cwd()}/setup/AllCards.csv`, output, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }).catch((e) => {
+    console.log('There was an error writing csv');
+    console.log(e);
+  });;
 }).catch((e) => {
   console.log('There was an error');
   console.log(e);
