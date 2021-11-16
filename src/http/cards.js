1ec9f6f6d968193233649b82daafa8c3ceca2be0
Mark Katerberg <katerberg@fastmail.fm>
2021-11-15 22:49:50 -0600

    404s for invalid names

--- src/http/cards.js
+++ src/http/cards.js
@@ -3,6 +3,9 @@ const {isValidCardName} = require('../daos/oracleDao');
 const cards = {
   getCards: async(request) => {
     const isValid = await isValidCardName(request.params.cardName);
+    if (!isValid) {
+      throw {statusCode: 404, message: 'Invalid card name'};
+    }
 
     return {isValid};
   },
