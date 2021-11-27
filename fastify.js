88915bad1a52b272cb2e5ed8af2d5b9bcaa003fe
Mark Katerberg <katerberg@fastmail.fm>
2021-11-27 13:28:25 -0600

    Allows fastify to run in docker

--- fastify.js
+++ fastify.js
@@ -12,7 +12,7 @@ Object.entries(fastifyRoutes.post).forEach(([name, route]) => {
 // Run the server!
 const start = async() => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(3000, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
