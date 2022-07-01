16e5757e183887b64c414b2fe88a9a0216ffafc5
Mark Katerberg <katerberg@fastmail.fm>
2022-07-01 11:12:49 -0500

    Ensures no negative lotusScores

--- src/daos/archivesDao.js
+++ src/daos/archivesDao.js
@@ -1,6 +1,6 @@
 const connection = require('./db');
 
-const lotusScoreSelect = 'SELECT * from ( ' +
+const lotusScoreSelect = 'SELECT card, averageRound, average, numberAvailable, numberTaken, ratio, COALESCE(NULLIF (ABS(lotusScore), -lotusScore), 0) as lotusScore from ( ' +
         'SELECT *, ((376-(((withRatio.numberAvailable - withRatio.numberTaken) * 376 + withRatio.average * withRatio.numberTaken) / withRatio.numberAvailable))/376*100) as lotusScore from ( ' +
           'SELECT a.card, a.averageRound, a.average, a.numberAvailable, a.numberTaken, a.numberTaken/a.numberAvailable as ratio from( ' +
             'SELECT archives.card ' +
